4579d129f1d68ac77b064937ed5cdb8887b46856
= <=>
2022-04-13 19:54:19 -0500

    Versión de reg,list y búsqueda de Producto, agregando GUI para el Login

---
+++ src/main/java/app/FrmLogin.java
@@ -0,0 +1,116 @@
+package app;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.Categoria;
+import model.Producto;
+import model.Usuario;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.util.List;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JComboBox;
+
+public class FrmLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtUsuario;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FrmLogin frame = new FrmLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FrmLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 146);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JButton btnNewButton = new JButton("Ingresar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				registrar();
+			}
+		});
+		btnNewButton.setBounds(324, 29, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setBounds(122, 30, 161, 20);
+		contentPane.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Usuario :");
+		lblNewLabel.setBounds(10, 33, 102, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblClave = new JLabel("Clave :");
+		lblClave.setBounds(10, 64, 102, 14);
+		contentPane.add(lblClave);
+		
+		txtClave = new JTextField();
+		txtClave.setColumns(10);
+		txtClave.setBounds(122, 61, 161, 20);
+		contentPane.add(txtClave);
+		
+	}
+
+	
+	private JTextField txtClave;
+	
+	
+	void registrar() {
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+		EntityManager em = fabrica.createEntityManager();
+
+		Query consulta = em.createNativeQuery("{call usp_validaAcceso(:usuario,:clave)}", Usuario.class);
+		consulta.setParameter("usuario", txtUsuario.getText());
+		consulta.setParameter("clave", txtClave.getText());
+		
+		Usuario u;
+		try {
+			u = (Usuario) consulta.getSingleResult();
+		} catch (Exception e) {
+			u = null;
+		}
+		
+		if (u == null) {
+			JOptionPane.showMessageDialog(null,"usuario o clave incorrecto");
+		} else {
+			JOptionPane.showMessageDialog(null,"Bienvenido..." + u.getNombre());
+		}
+		
+		em.close();
+	}
+}
